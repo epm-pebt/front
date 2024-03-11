24e35034cf92a4f2a83320d86b6c55dcc1d92997
Artur Bagautdinov <artur8888b@gmail.com>
2024-03-11 16:24:08 -0400

    textfield issues #2

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,9 +1,34 @@
+import TextField from './TextField/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import ErrorIcon from '@mui/icons-material/Error';
+import { StyledEngineProvider } from '@mui/material';
+
+// import { TextField } from "@mui/material";
+
 function HelloWorld() {
     return (
-        <div>
-            <h2>Hello world</h2>
-            <h4>Bottom text</h4>
-        </div>
+        <StyledEngineProvider injectFirst>
+            <div>
+                <h2>Hello world</h2>
+                <h4>Bottom text</h4>
+                <TextField
+                    placeholder="Ex.: Veggie Burger"
+                    adornment="startAdornment"
+                    position="start"
+                >
+                    <SearchIcon />
+                </TextField>
+                {/* <TextField
+                    placeholder="Error test"
+                    error
+                    adornment="endAdornment"
+                    position="end"
+                    helperText="Incorrect entry."
+                >
+                    <ErrorIcon />
+                </TextField> */}
+            </div>
+        </StyledEngineProvider>
     );
 }
 
--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { TextField as MUITextField } from '@mui/material';
+import { TextField as MUITextField, StyledEngineProvider } from '@mui/material';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import classNames from 'classnames';
 
-import './TextField.scss';
+import styles from './TextField.module.scss';
 
 const TextField = (props) => {
     const [isFocused, setIsFocused] = useState(false);
@@ -27,17 +27,22 @@ const TextField = (props) => {
     const onBlurHandler = () => setIsFocused(false);
 
     const textFieldClasses = classNames({
-        base: true,
-        error: error && !disabled ? true : false,
-        disabled: disabled,
-        focused: isFocused && !error,
+        [styles.base]: true,
+        [styles['MuiInputBase-root']]: true,
+        [styles.error]: error && !disabled ? true : false,
+        [styles.disabled]: disabled,
+        [styles.focused]: isFocused && !error,
     });
 
     return (
         <MUITextField
-            className={`${textFieldClasses} ${'customTextfield'}`}
+            className={textFieldClasses}
+            // classes={{
+            //     root: styles.base,
+            //     focused: styles.focusedInput,
+            // }}
             placeholder={placeholder}
-            label={isFocused ? placeholder : null}
+            // label={isFocused ? placeholder : null}
             // color={''}
             error={error}
             disabled={disabled}
@@ -46,6 +51,7 @@ const TextField = (props) => {
             onFocus={onFocusHandler}
             onBlur={onBlurHandler}
             InputProps={{
+                // className: styles['MuiFormControl-root'],
                 [adornment]: (
                     <InputAdornment position={position}>
                         {children}
@@ -53,90 +59,7 @@ const TextField = (props) => {
                 ),
             }}
         />
-
-    //     <TextField
-    //     placeholder="Ex.: Veggie Burger"
-    //     adornment="startAdornment"
-    //     position="start"
-    // >
-    //     <SearchIcon />
-    // </TextField>
-
-    // <TextField
-    //     placeholder="Error test"
-    //     error
-    //     adornment="endAdornment"
-    //     position="end"
-    //     helperText="Incorrect entry."
-    // >
-    //     <ErrorIcon />
-    </TextField>
-
-        // <MUITextField
-        //     id="outlined-basic"
-        //     // {isFocus ? []}
-        //     label={label}
-        //     // label={label || null}
-        //     // placeholder={placeholder}
-        //     placeholder={placeholder}
-        //     variant="outlined"
-        //     onFocus={onFocusHandler}
-        //     onBlur={onBlurHandler}
-        //     InputProps={{
-        //         startAdornment: (
-        //             // <InputAdornment style={{ marginTop: 0 }} position="start">
-        //             <InputAdornment position="start">
-        //                 <SearchIcon
-        //                 // sx={{
-        //                 //     width: 24,
-        //                 //     height: 24,
-        //                 // }}
-        //                 />
-        //             </InputAdornment>
-        //         ),
-        //     }}
-        // />
     );
-
-    // <MUITextField
-    //     id="outlined-basic"
-    //     // {isFocus ? []}
-    //     label={label || null}
-    //     // placeholder={placeholder}
-    //     placeholder={isFocused ? null : placeholder}
-    //     variant="outlined"
-    //     onFocus={onFocusHandler}
-    //     onBlur={onBlurHandler}
-    //     InputProps={{
-    //         startAdornment: (
-    //             // <InputAdornment style={{ marginTop: 0 }} position="start">
-    //             <InputAdornment position="start">
-    //                 <SearchIcon
-    //                 // sx={{
-    //                 //     width: 24,
-    //                 //     height: 24,
-    //                 // }}
-    //                 />
-    //             </InputAdornment>
-    //         ),
-    //     }}
-    // sx={{
-    //     width: 327,
-    //     color: '#6C6F80',
-    //     minHeight: 48,
-
-    //     '&.MuiInputBase-root': {
-    //         borderRadius: '8px',
-    //     },
-    //     '& input': {
-    //         padding: '0',
-    //         paddingRight: '12px',
-    //         paddingLeft: '8px',
-    //         fontFamily: 'Open-sans',
-    //         minHeight: '40px',
-    //     },
-    // }}
-    // className="customTextField"
 };
 
 TextField.propTypes = {
