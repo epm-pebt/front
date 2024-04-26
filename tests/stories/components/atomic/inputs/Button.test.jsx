6c5ef0121ad6961151c2ab4c1db89503e4e945e5
Eduardo Neto <Eduardo_Neto@epam.com>
2024-04-26 10:48:21 +0100

    added unit tests

---
+++ tests/stories/components/atomic/inputs/Button.test.jsx
@@ -0,0 +1,30 @@
+import { render } from '@testing-library/react';
+import Button from 'src/stories/components/atomic/inputs/Button';
+
+describe('Button', () => {
+    it('renders without crashing and displays the correct label', () => {
+        const { getByText } = render(<Button label="Test Button" />);
+        expect(getByText('Test Button')).toBeInTheDocument();
+    });
+
+    it('applies correct variant for primary', () => {
+        const { getByRole } = render(
+            <Button label="Test Button" variant="primary" />
+        );
+        expect(getByRole('button')).toHaveClass('MuiButton-primary');
+    });
+
+    it('applies correct variant for secondary', () => {
+        const { getByRole } = render(
+            <Button label="Test Button" variant="secondary" />
+        );
+        expect(getByRole('button')).toHaveClass('MuiButton-secondary');
+    });
+
+    it('applies correct variant for tertiary', () => {
+        const { getByRole } = render(
+            <Button label="Test Button" variant="tertiary" />
+        );
+        expect(getByRole('button')).toHaveClass('MuiButton-tertiary');
+    });
+});
