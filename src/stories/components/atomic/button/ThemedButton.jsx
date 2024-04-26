bcda8755b8a0453e527bbe6bf93daa849103373f
Eduardo Neto <Eduardo_Neto@epam.com>
2024-04-26 10:42:49 +0100

    removed unnecessary files

--- src/stories/components/atomic/button/ThemedButton.jsx
+++
@@ -1,5 +0,0 @@
-import MuiButton from '@mui/material/Button';
-
-const ThemedButton = (props) => <MuiButton {...props} disableElevation disableFocusRipple disableRipple>{props.label}</MuiButton>;
-
-export default ThemedButton;
